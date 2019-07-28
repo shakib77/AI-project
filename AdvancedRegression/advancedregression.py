838416e69efc207b49440660e9a3e7e93cd4ccd5
shakib77 <shakibshaon77@gmail.com>
2019-07-28 22:12:55 +0600

    2nd part of the code

--- AdvancedRegression/advancedregression.py
+++ AdvancedRegression/advancedregression.py
@@ -39,3 +39,45 @@ houses.select_dtypes(include=['int','float']).isnull().sum()[houses.select_dtype
 for col in ('MasVnrArea','BsmtFinSF1','BsmtFinSF2','BsmtUnfSF','TotalBsmtSF','BsmtFullBath','BsmtHalfBath','GarageYrBlt','GarageCars','GarageArea'):
     train[col]=train[col].fillna(0)
     test[col]=test[col].fillna(0)
+ 
+train['LotFrontage']=train['LotFrontage'].fillna(train['LotFrontage'].mean())
+test['LotFrontage']=test['LotFrontage'].fillna(train['LotFrontage'].mean())
+
+print(train.isnull().sum().sum())
+print(train.isnull().sum().sum())
+
+plt.figure(figsize=[30,15])
+sns.heatmap(train.corr(), annot=True)
+
+#from 2 features high correlated, removing the less correlated with SalePrice
+train.drop(['GarageArea','1stFlrSF','TotRmsAbvGrd','2ndFlrSF'], axis=1, inplace=True)
+test.drop(['GarageArea','1stFlrSF','TotRmsAbvGrd','2ndFlrSF'], axis=1, inplace=True)
+
+#removing outliers recomended by author
+train = train[train['GrLivArea']<4000]
+
+len_train=train.shape[0]
+print(train.shape)
+
+houses=pd.concat([train,test], sort=False)
+
+houses['MSSubClass']=houses['MSSubClass'].astype(str)
+
+skew=houses.select_dtypes(include=['int','float']).apply(lambda x: skew(x.dropna())).sort_values(ascending=False)
+skew_df=pd.DataFrame({'Skew':skew})
+skewed_df=skew_df[(skew_df['Skew']>0.5)|(skew_df['Skew']<-0.5)]
+
+skewed_df.index
+
+train=houses[:len_train]
+test=houses[len_train:]
+
+lam=0.1
+for col in ('MiscVal', 'PoolArea', 'LotArea', 'LowQualFinSF', '3SsnPorch',
+       'KitchenAbvGr', 'BsmtFinSF2', 'EnclosedPorch', 'ScreenPorch',
+       'BsmtHalfBath', 'MasVnrArea', 'OpenPorchSF', 'WoodDeckSF',
+       'LotFrontage', 'GrLivArea', 'BsmtFinSF1', 'BsmtUnfSF', 'Fireplaces',
+       'HalfBath', 'TotalBsmtSF', 'BsmtFullBath', 'OverallCond', 'YearBuilt',
+       'GarageYrBlt'):
+    train[col]=boxcox1p(train[col],lam)
+    test[col]=boxcox1p(test[col],lam)
